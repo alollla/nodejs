9291bc9ac394dee2c313d58e8e20c6842f754e12
alollla <alolllabatoev@gmail.com>
2024-08-31 00:52:00 +0700

    feat: handlebars

--- app.js
+++ app.js
@@ -1,10 +1,28 @@
-const express = require("express"); 
-
+const express = require("express");
+const hbs = require("hbs");
+  
 const app = express();
- 
-app.get("/", function(_, response){
- 
-    response.end("Hello METANIT.COM");
+  
+hbs.registerHelper("getTime", function(){
+      
+    const myDate = new Date();
+    const hour = myDate.getHours();
+    let minute = myDate.getMinutes();
+    let second = myDate.getSeconds();
+    if (minute < 10) {
+        minute = "0" + minute;
+    }
+    if (second < 10) {
+        second = "0" + second;
+    }
+    return `Текущее время: ${hour}:${minute}:${second}`;
 });
-
-app.listen(3000, function(){ console.log("Сервер начал принимать запросы по адресу http://localhost:3000")});
+  
+app.set("view engine", "hbs");
+  
+app.get("/", function(_, response){
+      
+    response.render("home.hbs");
+});
+  
+app.listen(3000);
